df2b0e95ab7a3684fd4a9112d02956ca84ecb604
tetreum <basura@yelidmod.com>
2016-12-09 13:45:24 +0100

    + Detect scene changes

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
@@ -13,12 +14,14 @@ public class Menu : MonoBehaviour
     public void Start ()
     {
         Instance = this;
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
 
         // make panel list more easier to access
         foreach (GameObject panel in panelList) {
             panels.Add(panel.name, panel);
         }
         panelList = null;
+
     }
 
     public void showPanel(string name, bool hideAll = true)
@@ -35,4 +38,17 @@ public class Menu : MonoBehaviour
     public void hidePanel(string name) {
         panels[name + "Panel"].SetActive(false);
     }
-}
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        switch(scene.name)
+        {
+            case "Main":
+                showPanel("Main");
+                break;
+            case "Level1":
+                hidePanel("Main");
+                break;
+        }
+    }
+ }
